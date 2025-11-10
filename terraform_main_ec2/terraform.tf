da1f5e5a437a6dc95020b0eeae73203138caf3d1
Teju-Aws <tejaswinigundluru@gmail.com>
2025-11-10 19:17:59 +0530

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -5,13 +5,6 @@ terraform {
         version = ">= 5.25.0"
     }
   }
-
-  backend "s3" {
-    bucket = "nareshawsdevvvv"
-    key    = "ec2/terraform.tfstate"
-    region = "us-east-1"
-  }
-
   required_version = ">= 1.6.3"
 }
 provider "aws" {
